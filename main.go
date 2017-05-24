5499a6a8803e8c16f829edcf91ffc274f6d1df6f
Joseph Salisbury <salisbury.joseph@gmail.com>
2017-05-24 16:58:43 +0100

    Adds microkit skeleton

---
+++ main.go
@@ -0,0 +1,101 @@
+package main
+
+import (
+	"os"
+
+	"github.com/spf13/viper"
+
+	"github.com/giantswarm/microkit/command"
+	"github.com/giantswarm/microkit/logger"
+	microserver "github.com/giantswarm/microkit/server"
+
+	"github.com/giantswarm/draughtsman/flag"
+	"github.com/giantswarm/draughtsman/server"
+	"github.com/giantswarm/draughtsman/service"
+)
+
+var (
+	description string     = "draughtsman is an in-cluster agent that handles Helm based deployments"
+	f           *flag.Flag = flag.New()
+	gitCommit   string     = "n/a"
+	name        string     = "draughtsman"
+	source      string     = "https://github.com/giantswarm/draughtsman"
+)
+
+func main() {
+	var err error
+
+	// Create a new logger which is used by all packages.
+	var newLogger logger.Logger
+	{
+		loggerConfig := logger.DefaultConfig()
+		loggerConfig.IOWriter = os.Stdout
+		newLogger, err = logger.New(loggerConfig)
+		if err != nil {
+			panic(err)
+		}
+	}
+
+	// We define a server factory to create the custom server once all command
+	// line flags are parsed and all microservice configuration is storted out.
+	newServerFactory := func(v *viper.Viper) microserver.Server {
+		// Create a new custom service which implements business logic.
+		var newService *service.Service
+		{
+			serviceConfig := service.DefaultConfig()
+
+			serviceConfig.Flag = f
+			serviceConfig.Logger = newLogger
+			serviceConfig.Viper = v
+
+			serviceConfig.Description = description
+			serviceConfig.GitCommit = gitCommit
+			serviceConfig.Name = name
+			serviceConfig.Source = source
+
+			newService, err = service.New(serviceConfig)
+			if err != nil {
+				panic(err)
+			}
+		}
+
+		// Create a new custom server which bundles our endpoints.
+		var newServer microserver.Server
+		{
+			serverConfig := server.DefaultConfig()
+
+			serverConfig.MicroServerConfig.Logger = newLogger
+			serverConfig.MicroServerConfig.ServiceName = name
+			serverConfig.MicroServerConfig.Viper = v
+			serverConfig.Service = newService
+
+			newServer, err = server.New(serverConfig)
+			if err != nil {
+				panic(err)
+			}
+		}
+
+		return newServer
+	}
+
+	// Create a new microkit command which manages our custom microservice.
+	var newCommand command.Command
+	{
+		commandConfig := command.DefaultConfig()
+
+		commandConfig.Logger = newLogger
+		commandConfig.ServerFactory = newServerFactory
+
+		commandConfig.Description = description
+		commandConfig.GitCommit = gitCommit
+		commandConfig.Name = name
+		commandConfig.Source = source
+
+		newCommand, err = command.New(commandConfig)
+		if err != nil {
+			panic(err)
+		}
+	}
+
+	newCommand.CobraCommand().Execute()
+}
