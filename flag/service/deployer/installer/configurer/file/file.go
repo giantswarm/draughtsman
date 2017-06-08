eecadf83b65177529611006113816a5b88c50a65
Joseph Salisbury <salisbury.joseph@gmail.com>
2017-06-08 12:34:31 +0100

    Adds Configurer spec, and FileConfigurer

---
+++ flag/service/deployer/installer/configurer/file/file.go
@@ -0,0 +1,5 @@
+package file
+
+type File struct {
+	Path string
+}
--- flag/service/deployer/installer/installer.go
+++ flag/service/deployer/installer/installer.go
@@ -1,10 +1,12 @@
 package installer
 
 import (
+	"github.com/giantswarm/draughtsman/flag/service/deployer/installer/configurer"
 	"github.com/giantswarm/draughtsman/flag/service/deployer/installer/helm"
 )
 
 type Installer struct {
-	Helm helm.Helm
-	Type string
+	Helm       helm.Helm
+	Configurer configurer.Configurer
+	Type       string
 }
