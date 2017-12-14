65d0260c7142879d6b64ed9ac435e173ac7e4c5d
Marian Steinbach <marian@giantswarm.io>
2017-12-14 22:37:29 +0100

    Adding TransactionResponder to config
    Shamelessly copying from https://github.com/giantswarm/ingress-operator/commit/a93a6da09412631ff8f0c21da61fb2308ca5090c

--- main.go
+++ main.go
@@ -12,7 +12,10 @@ import (
 
 	"github.com/giantswarm/microkit/command"
 	microserver "github.com/giantswarm/microkit/server"
+	"github.com/giantswarm/microkit/transaction"
 	"github.com/giantswarm/micrologger"
+	"github.com/giantswarm/microstorage"
+	"github.com/giantswarm/microstorage/memory"
 	"github.com/giantswarm/operatorkit/client/k8s"
 
 	"github.com/giantswarm/draughtsman/flag"
@@ -112,6 +115,26 @@ func main() {
 			}
 		}
 
+		var storage microstorage.Storage
+		{
+			storage, err = memory.New(memory.DefaultConfig())
+			if err != nil {
+				panic(err)
+			}
+		}
+
+		var transactionResponder transaction.Responder
+		{
+			c := transaction.DefaultResponderConfig()
+			c.Logger = newLogger
+			c.Storage = storage
+
+			transactionResponder, err = transaction.NewResponder(c)
+			if err != nil {
+				panic(err)
+			}
+		}
+
 		// Create a new custom server which bundles our endpoints.
 		var newServer microserver.Server
 		{
@@ -119,6 +142,7 @@ func main() {
 
 			serverConfig.MicroServerConfig.Logger = newLogger
 			serverConfig.MicroServerConfig.ServiceName = name
+			serverConfig.MicroServerConfig.TransactionResponder = transactionResponder
 			serverConfig.MicroServerConfig.Viper = v
 			serverConfig.Service = newService
 
