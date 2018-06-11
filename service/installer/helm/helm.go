9dcc35cac4084835d1d56e8f15e154316b2f188e
Oliver Ponder <oliver.ponder@gmail.com>
2018-06-11 12:41:19 +0800

    Remove --recreate-pods from helm Install command.
    This flag restarts all pods immediately on deploy. The consequence is
    that we do not get zero down time deploys.
    
    See: https://github.com/giantswarm/giantswarm/issues/3366

--- service/installer/helm/helm.go
+++ service/installer/helm/helm.go
@@ -273,7 +273,7 @@ func (i *HelmInstaller) Install(event eventerspec.DeploymentEvent) error {
 	//
 	var installCommand []string
 	{
-		installCommand = append(installCommand, "upgrade", "--install", "--recreate-pods")
+		installCommand = append(installCommand, "upgrade", "--install")
 		installCommand = append(installCommand, valuesFilesArgs...)
 		installCommand = append(installCommand, project, chartPath)
 
