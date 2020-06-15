f2b845846a8d6d9a115f8194fd588c0f1595bdd3
Jihyuk Bok <jhbok@giantswarm.io>
2020-06-15 10:17:34 +0200

    moving project list into the project pkg

--- pkg/project/list.go
+++ pkg/project/list.go
@@ -1,1 +1,43 @@
 package project
+
+var (
+	commonProjectList = []string{
+		"cluster-operator",
+		"draughtsman",
+	}
+
+	providerProjectList = map[string][]string{
+		"aws": {
+			"aws-app-collection",
+			"aws-operator",
+		},
+		"kvm": {
+			"kvm-app-collection",
+			"kvm-operator",
+		},
+		"azure": {
+			"azure-app-collection",
+			"azure-operator",
+		},
+	}
+
+	perInstallationProjectLists = map[string][]string{
+		"gaia": {"app-operator"},
+		"gauss": {
+			"app-operator",
+		},
+		"geckon": {"app-operator"},
+		"ghost":  {"app-operator"},
+		"ginger": {"app-operator"},
+		"giraffe": {
+			"app-operator",
+		},
+		"godsmack": {"app-operator"},
+		"gorgoth":  {"app-operator"},
+	}
+)
+
+func GetProjectList(provider, installation string) []string {
+	list := append(commonProjectList, providerProjectList[provider]...)
+	return append(list, perInstallationProjectLists[installation]...)
+}
